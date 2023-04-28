30d32c5522d1a733b52f0254495dff31b2db5fd9
prajwal <prajwal.siwa1@gmail.com>
2023-04-28 11:45:45 +0545

    final codeEditor

---
+++ app.js
@@ -0,0 +1,78 @@
+var htmlEditor = ace.edit("html");
+htmlEditor.setTheme("ace/theme/cobalt");
+htmlEditor.session.setMode("ace/mode/html");
+htmlEditor.resize();
+htmlEditor.setHighlightActiveLine(false);
+htmlEditor.setShowPrintMargin(false);
+
+
+var cssEditor = ace.edit("css");
+cssEditor.setTheme("ace/theme/cobalt");
+cssEditor.session.setMode("ace/mode/css");
+cssEditor.resize();
+cssEditor.setHighlightActiveLine(false);
+cssEditor.setShowPrintMargin(false);
+
+
+
+
+var jsEditor = ace.edit("js");
+jsEditor.setTheme("ace/theme/cobalt");
+jsEditor.session.setMode("ace/mode/javascript");
+jsEditor.resize();
+jsEditor.setHighlightActiveLine(false);
+jsEditor.setShowPrintMargin(false);
+
+
+function compiler() {
+ var htmlValue = htmlEditor.getValue();
+ var cssValue = cssEditor.getValue();
+ var jsValue = jsEditor.getValue();
+ var result = document.getElementById("result").contentWindow.document;
+
+
+ result.open();
+ result.writeln(
+   "<style>" +
+   cssValue +
+   "</style>" +
+   htmlValue +
+   "<script>" +
+   jsValue +
+   "</script>"
+ );
+ result.close();
+}
+
+
+var allButtons = document.querySelectorAll("#button-wrapper button");
+var allPanels = document.querySelectorAll("#ide-container .panel-wrapper");
+
+
+function switchPanel(panelIndex) {
+ switcher(panelIndex);
+}
+
+
+switchPanel(0);
+
+
+function runEdit(panelIndex) {
+ switcher(panelIndex);
+ compiler();
+}
+
+
+function switcher(panelIndex) {
+ allButtons.forEach(function (node) {
+   node.style.background = "";
+ });
+ allButtons[panelIndex].style.background = "#002240";
+ allPanels.forEach(function (node) {
+   node.style.display = "none";
+ });
+ allPanels[panelIndex].style.display = "block";
+}
+
+
+
